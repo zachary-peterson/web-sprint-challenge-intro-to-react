8b151de12050e610248e368ecf83a2891c3d5842
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-17 12:52:42 -0400

    MVP without questions

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characters, setCharacters] = useState([]);
+  const [charId, setCharId] = useState('');
+
+  const starWarsChars = 'https://swapi.dev/api/people/'
+
+  useEffect(() => {
+    axios.get(starWarsChars)
+    .then(res => {
+      console.log(res.data.results);
+      setCharacters(res.data.results);
+    })
+    .catch(err => {
+      console.dir(err);
+    })
+  }, [])
+
+  console.log(characters);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,9 +31,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+        <Character info={characters}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,39 @@
-// Write your Character component here
+import React, { useState, useEffect } from 'react';
+import App from '../App';
+import axios from 'axios';
+import styled from 'styled-components'
+import '../App.css'
+
+
+
+//Write your Character component here
+export default function Character(props){
+
+const info = props.info;
+
+const StyledChar = styled.div`
+    width: 75%;
+    background-color: #FFFFFF;
+    color: #000000;
+    text-align: center;
+    margin: 1% auto;
+
+    button {
+        margin: 1%;
+        background-color: #000000;
+        color: #FFFFFF;
+    }
+`
+
+console.log(props.info)
+
+return info.map(ch => {
+    return (
+    <StyledChar key={ch.name}>
+        <p>{ch.name}</p>
+        <p>{ch['birth_year']}</p>
+    </StyledChar>
+);
+})
+}
+
